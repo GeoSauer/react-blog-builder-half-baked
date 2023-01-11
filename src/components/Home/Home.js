da7a9ec30d3481ce70d0d814b2c42ab2409fdd25
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-11 13:22:01 -0700

    add title state variable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -7,12 +8,14 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('');
+  // const newTitle = (event) => setTitle(event.target.value);
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} setTitle={setTitle} />
+      <Editor title={title} setTitle={setTitle} />
     </main>
   );
 }
